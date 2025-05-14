96be0057bb38644ca353c140f927ab68899f4ffc
Krisbrn <samucaaraujoalmeida@gmail.com>
2025-05-14 11:01:12 -0300

    Add files via upload

---
+++ Aula 14/samuel.js
@@ -0,0 +1,37 @@
+<script>
+*/
+Bloco de código JavaScript
+/*
+
+// Linha comentada...
+
+var 1 = 1;
+
+</script>
+
+var 1 = 1;
+
+switch(i)
+{
+    case 0:
+        break;
+        case 1:
+            break
+
+}
+
+// for(inicialização; condição; complemento)
+
+for(i= 0; i< 10; i++){
+    document.write(i + '');
+}
+
+// resultado: 0 1 2 3 4 5 6 7 8 9
+
+var A;
+let b=20;
+const c=40;
+
+if(true){
+    let T=20;
+}
